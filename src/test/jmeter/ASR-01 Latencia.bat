ea43d4e65b6fa357d8eb63822304096ba8ad9f24
elviejoalejo <elviejoalejo@gmail.com>
2025-02-23 18:37:52 -0500

    Test Cases update

--- src/test/jmeter/ASR-01 Latencia.bat
+++ src/test/jmeter/ASR-01 Latencia.bat
@@ -1,12 +1,12 @@
 ECHO OFF
 
-SET ITERATIONS=60
+SET ITERATIONS=2
 
 FOR /L %%i IN (1,1,%ITERATIONS%) DO (
 	for /f %%J in ('powershell Get-Random -Minimum 100 -Maximum 700') do (
-		Echo Ejecucion #%%i/%ITERATIONS%, testDuration=%%J
+		Echo Ejecucion #%%i/%ITERATIONS%, initialThreadPool=%%J
 		ECHO on
-		CALL jmeter -J testDuration=%%J -n -t "C:\Uniandes\machineengine\src\test\jmeter\ASR-01 Latencia.jmx" -l C:\Uniandes\machineengine\src\test\jmeter\output\ASR-01-log%%i.csv
+		CALL jmeter -J initialThreadPool=%%J -n -t ".\ASR-01 Latencia.jmx" -l .\output\ASR-01-log%%i.csv
 		ECHO OFF
 	)
 )
